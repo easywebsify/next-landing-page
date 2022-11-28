424f09ac55dece2d7666f87c04faa746aa1cd1e5
easywebsify <80096561+easywebsify@users.noreply.github.com>
2022-11-28 16:58:17 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Container,
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
 } from 'reactstrap';
 
 const Header = () => {
@@ -20,39 +19,37 @@ const Header = () => {
   });
 
   const handleScroll = () => {
-    if (window.scrollY > 90) {
+    if (window.scrollY > 180) {
       setSticky(true);
-    } else if (window.scrollY < 90) {
+    } else if (window.scrollY < 180) {
       setSticky(false);
     }
   }
 
   return (
-    <div className={`header${sticky ? ' sticky' : ''}`}>
-      <Navbar light expand="md">
-        <Container>
-          <NavbarBrand href="/">LOGO</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="m-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#feature">Features</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#service">Services</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#about">About</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+    <Navbar color="light" light container="md" expand="md" sticky={sticky ? "top" : ""}>
+      <NavbarBrand href="/">LOGO</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <Nav className="m-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#feature">Features</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#service">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  )
 }
 
 export default Header;
